ce3748053805e3cf3e16152acf2dfceca290e970
nizar607 <nizar.ouertani@esprit.tn>
2024-02-26 10:34:02 +0100

    second commit

--- src/main/java/tn/esprit/spring/TimesheetDevopsApplication.java
+++ src/main/java/tn/esprit/spring/TimesheetDevopsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TimesheetDevopsApplication {
 
+
+	//another comment
 	public static void main(String[] args) {
 		SpringApplication.run(TimesheetDevopsApplication.class, args);
 	}
